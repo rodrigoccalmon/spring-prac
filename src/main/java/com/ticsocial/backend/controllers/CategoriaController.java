d3525fb8eb089554144cd5bd203925751f77a707
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-02-16 17:41:34 -0300

    update postregresql, pom, swagger, metodo insert e find no swagger ok

--- src/main/java/com/ticsocial/backend/controllers/CategoriaController.java
+++ src/main/java/com/ticsocial/backend/controllers/CategoriaController.java
@@ -16,29 +16,34 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ticsocial.backend.DTO.CategoriaDTO;
 import com.ticsocial.backend.entities.Categoria;
+import com.ticsocial.backend.repositories.CategoriaRepository;
 import com.ticsocial.backend.services.CategoriaService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping(value = "api/categorias")
 public class CategoriaController {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
+	@Autowired
+	private CategoriaRepository repo;
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@ApiOperation(value = "GET categoria via ID")
 	public ResponseEntity<Optional<Categoria>> findById(@PathVariable Integer id) {
 		Optional<Categoria> obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert (@Valid @RequestBody CategoriaDTO objDto) {
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
 		Categoria obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 }
