ad01ece35e6d001555236e6e89955f1102cba045
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-02-16 17:27:10 -0300

    post de categoria com DTO ok

--- src/main/java/com/ticsocial/backend/controllers/CategoriaController.java
+++ src/main/java/com/ticsocial/backend/controllers/CategoriaController.java
@@ -1,14 +1,20 @@
 package com.ticsocial.backend.controllers;
 
+import java.net.URI;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ticsocial.backend.DTO.CategoriaDTO;
 import com.ticsocial.backend.entities.Categoria;
 import com.ticsocial.backend.services.CategoriaService;
 
@@ -25,4 +31,14 @@ public class CategoriaController {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert (@Valid @RequestBody CategoriaDTO objDto) {
+		Categoria obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
