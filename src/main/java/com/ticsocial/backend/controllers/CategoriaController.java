89d21bb92503764e6c9085d5bbbe0a0ece239863
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-02-16 16:57:32 -0300

    metodo get id categorias ok

--- src/main/java/com/ticsocial/backend/controllers/CategoriaController.java
+++ src/main/java/com/ticsocial/backend/controllers/CategoriaController.java
@@ -1,7 +1,28 @@
 package com.ticsocial.backend.controllers;
 
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ticsocial.backend.entities.Categoria;
+import com.ticsocial.backend.services.CategoriaService;
+
+@RestController
+@RequestMapping(value = "api/categorias")
 public class CategoriaController {
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Optional<Categoria>> findById(@PathVariable Integer id) {
+		Optional<Categoria> obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 
 }
--- src/main/java/com/ticsocial/backend/services/CategoriaService.java
+++ src/main/java/com/ticsocial/backend/services/CategoriaService.java
@@ -1,5 +1,25 @@
 package com.ticsocial.backend.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ticsocial.backend.entities.Categoria;
+import com.ticsocial.backend.repositories.CategoriaRepository;
+
+@Service
 public class CategoriaService {
 
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Optional<Categoria> findById(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj;
+	}
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 }
