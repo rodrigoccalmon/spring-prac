dc8bcb82edd709b26c885c1ed221d49cb4c61dd8
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-02-16 16:21:11 -0300

    projeto criado

---
+++ src/test/java/com/ticsocial/backend/SpringPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticsocial.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
